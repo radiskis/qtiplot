f57f836e3051909ac58740f7a2d6847e20a879c6
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2007-12-15 11:08:03 +0000

    - Fixed guessInitialValues() in GaussAmpFit.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@784 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/src/globals.h
+++ qtiplot/src/globals.h
@@ -35,8 +35,8 @@ const int min_version = 9;
 //! Patch version number (0..9)
 const int patch_version = 3;
 //! Extra version information string (like "alpha", "-2", etc...)
-const char * extra_version = "rc1";
+const char * extra_version = "-rc1";
 //! Copyright string containing the author names
-const char * copyright_string = "Copyright (C) 2004-2007 Ion Vasilief\nCopyright (C) 2006 - june 2007 Tilman Hoener zu Siederdissen, Knut Franke";
+const char * copyright_string = "Copyright (C) 2004-2007 Ion Vasilief";
 //! Release date as a string
 const char * release_date = " 2007/12/16";
