af15bb78d811ba28b12ccc2d16e51c1d02b15b22
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2008-03-05 09:28:45 +0000

    Fixed crash when executing custom script actions.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@857 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/src/globals.h
+++ qtiplot/src/globals.h
@@ -35,8 +35,8 @@ const int min_version = 9;
 //! Patch version number (0..9)
 const int patch_version = 3;
 //! Extra version information string (like "alpha", "-2", etc...)
-const char * extra_version = "";
+const char * extra_version = "-2";
 //! Copyright string containing the author names
 const char * copyright_string = "Copyright (C) 2004-2008 Ion Vasilief";
 //! Release date as a string
-const char * release_date = " 2008/03/04";
+const char * release_date = " 2008/03/05";
