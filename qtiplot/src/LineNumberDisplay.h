da7c696e6ea70c6c3f20fb59b5af39cd8d2bda2b
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2008-05-09 10:54:33 +0000

    Added forgotten new files.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@944 5a6a7de5-fb12-0410-b871-c33778c25c60

---
+++ qtiplot/src/LineNumberDisplay.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+    File                 : LineNumberDisplay.h
+    Project              : QtiPlot
+    --------------------------------------------------------------------
+    Copyright            : (C) 2008 by Ion Vasilief
+    Email (use @ for *)  : ion_vasilief*yahoo.fr
+    Description          : Q widget displaying line numbers for a QTextEdit
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef LineNumberDisplay_H
+#define LineNumberDisplay_H
+
+#include <QTextEdit>
+
+class LineNumberDisplay: public QTextEdit
+{
+    Q_OBJECT
+
+public:
+	LineNumberDisplay(QTextEdit *te, QWidget *parent = 0);
+
+public slots:
+	void updateLineNumbers();
+	
+private:
+	void showEvent(QShowEvent *);
+	QTextEdit *d_text_edit;
+};
+
+#endif
