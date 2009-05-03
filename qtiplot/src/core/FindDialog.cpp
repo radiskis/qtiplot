5b74e2128baa8f5c4b25810335e2f988fc343636
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2009-05-03 23:54:55 +0000

    Improved find window dialog.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@1243 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/src/core/FindDialog.cpp
+++ qtiplot/src/core/FindDialog.cpp
@@ -5,7 +5,7 @@
     Copyright            : (C) 2006 by Ion Vasilief, Tilman Hoener zu Siederdissen
     Email (use @ for *)  : ion_vasilief*yahoo.fr, thzs*gmx.net
     Description          : Find dialog
-                           
+
  ***************************************************************************/
 
 /***************************************************************************
@@ -48,7 +48,7 @@ FindDialog::FindDialog( QWidget* parent, Qt::WFlags fl )
 
 	QGridLayout * topLayout = new QGridLayout();
 	QGridLayout * bottomLayout = new QGridLayout();
-	
+
 	topLayout->addWidget( new QLabel(tr( "Start From" )), 0, 0 );
 	labelStart = new QLabel();
 	labelStart->setFrameStyle(QFrame::Panel | QFrame::Sunken);
@@ -66,22 +66,21 @@ FindDialog::FindDialog( QWidget* parent, Qt::WFlags fl )
 	boxFind->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed));
 	topLayout->addWidget( boxFind, 1, 1, 1, 4 );
 
-
 	QGroupBox * groupBox = new QGroupBox(tr("Search in"));
 	QVBoxLayout * groupBoxLayout = new QVBoxLayout( groupBox );
 
 	boxWindowNames = new QCheckBox(tr("&Window Names"));
     boxWindowNames->setChecked(true);
 	groupBoxLayout->addWidget(boxWindowNames);
-	
+
     boxWindowLabels = new QCheckBox(tr("Window &Labels"));
     boxWindowLabels->setChecked( false );
 	groupBoxLayout->addWidget(boxWindowLabels);
-	
+
     boxFolderNames = new QCheckBox(tr("Folder &Names"));
     boxFolderNames->setChecked( false );
 	groupBoxLayout->addWidget(boxFolderNames);
-	
+
 	bottomLayout->addWidget( groupBox, 0, 0, 3, 1 );
 
 	boxCaseSensitive = new QCheckBox(tr("Case &Sensitive"));
@@ -91,26 +90,28 @@ FindDialog::FindDialog( QWidget* parent, Qt::WFlags fl )
     boxPartialMatch = new QCheckBox(tr("&Partial Match Allowed"));
     boxPartialMatch->setChecked(true);
 	bottomLayout->addWidget( boxPartialMatch, 1, 1 );
-	
+
 	boxSubfolders = new QCheckBox(tr("&Include Subfolders"));
     boxSubfolders->setChecked(true);
 	bottomLayout->addWidget( boxSubfolders, 2, 1 );
-	
+
 	buttonFind = new QPushButton(tr("&Find"));
     buttonFind->setDefault( true );
 	bottomLayout->addWidget( buttonFind, 0, 2 );
-   
+
 	buttonReset = new QPushButton(tr("&Update Start Path"));
 	bottomLayout->addWidget( buttonReset, 1, 2 );
     buttonCancel = new QPushButton(tr("&Close"));
 	bottomLayout->addWidget( buttonCancel, 2, 2 );
+	bottomLayout->setColumnStretch(4, 1);
 
 	QVBoxLayout* mainLayout = new QVBoxLayout(this);
 	mainLayout->addLayout(topLayout);
 	mainLayout->addLayout(bottomLayout);
+	mainLayout->addStretch();
 
 	setStartPath();
-   
+
     // signals and slots connections
     connect( buttonFind, SIGNAL( clicked() ), this, SLOT( accept() ) );
 	connect( buttonReset, SIGNAL( clicked() ), this, SLOT( setStartPath() ) );
@@ -131,13 +132,9 @@ void FindDialog::accept()
 			boxSubfolders->isChecked());
 	// add the combo box's current text to the list when the find button is pressed
 	QString text = boxFind->currentText();
-	if(!text.isEmpty()) 
-	{
-		if(boxFind->findText(text) == -1) // no duplicates
-		{
+	if(!text.isEmpty() && boxFind->findText(text) == -1){ // no duplicates
 			boxFind->insertItem(0, text);
 			boxFind->setCurrentIndex(0);
-		}
 	}
 }
 
