a925180113499f35f20aa917c34d3503d4f0ae40
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2010-04-01 11:26:51 +0000

    Added copyright/license header to some files.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@1656 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/python-includepath.py
+++ qtiplot/python-includepath.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python
+
+############################################################################
+#                                                                          #
+# File                 : python-includepath.py                             #
+# Project              : QtiPlot                                           #
+# Description          : dump -I paths for Python and SIP                  #
+# Copyright            : (C) 2007-2009 Knut Franke (knut.franke*gmx.de)    #
+#                        (replace * with @ in the email address)           #
+#                                                                          #
+############################################################################
+#                                                                          #
+#  This program is free software; you can redistribute it and/or modify    #
+#  it under the terms of the GNU General Public License as published by    #
+#  the Free Software Foundation; either version 2 of the License, or       #
+#  (at your option) any later version.                                     #
+#                                                                          #
+#  This program is distributed in the hope that it will be useful,         #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#  GNU General Public License for more details.                            #
+#                                                                          #
+#   You should have received a copy of the GNU General Public License      #
+#   along with this program; if not, write to the Free Software            #
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor,                     #
+#   Boston, MA  02110-1301  USA                                            #
+#                                                                          #
+############################################################################
+
 from distutils import sysconfig
 print sysconfig.get_python_inc()
