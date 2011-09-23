d791722fd58d4a98acbc2be90ef246496c65e4cb
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2011-09-23 16:51:10 +0000

    - Fixed speed issues in matrices. - Removed matrix size limitations in MatrixSizeDialog.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@2267 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/src/matrix/MatrixSizeDialog.cpp
+++ qtiplot/src/matrix/MatrixSizeDialog.cpp
@@ -2,7 +2,7 @@
     File                 : MatrixSizeDialog.cpp
     Project              : QtiPlot
     --------------------------------------------------------------------
-    Copyright            : (C) 2004-2008 by Ion Vasilief
+	Copyright            : (C) 2004 - 2011 by Ion Vasilief
     Email (use @ for *)  : ion_vasilief*yahoo.fr
     Description          : Matrix dimensions dialog
 
@@ -26,8 +26,8 @@
  *   Boston, MA  02110-1301  USA                                           *
  *                                                                         *
  ***************************************************************************/
-#include "MatrixSizeDialog.h"
-#include "MatrixCommand.h"
+#include <MatrixSizeDialog.h>
+#include <MatrixCommand.h>
 
 #include <DoubleSpinBox.h>
 
@@ -39,77 +39,77 @@
 #include <QLayout>
 
 MatrixSizeDialog::MatrixSizeDialog( Matrix *m, QWidget* parent, Qt::WFlags fl )
-    : QDialog( parent, fl ),
-    d_matrix(m)
+	: QDialog( parent, fl ),
+	d_matrix(m)
 {
-    setWindowTitle(tr("QtiPlot - Matrix Dimensions"));
-    setAttribute(Qt::WA_DeleteOnClose);
+	setWindowTitle(tr("QtiPlot - Matrix Dimensions"));
+	setAttribute(Qt::WA_DeleteOnClose);
 	setSizeGripEnabled(true);
 
 	groupBox1 = new QGroupBox(tr("Dimensions"));
 	QHBoxLayout *topLayout = new QHBoxLayout(groupBox1);
 	topLayout->addWidget( new QLabel(tr( "Rows" )) );
-    boxRows = new QSpinBox();
-	boxRows->setRange(1, 1000000);
+	boxRows = new QSpinBox();
+	boxRows->setRange(1, INT_MAX);
 	topLayout->addWidget(boxRows);
 	topLayout->addStretch();
 	topLayout->addWidget( new QLabel(tr( "Columns" )) );
-    boxCols = new QSpinBox();
-	boxCols->setRange(1, 1000000);
+	boxCols = new QSpinBox();
+	boxCols->setRange(1, INT_MAX);
 	topLayout->addWidget(boxCols);
 
-    groupBox2 = new QGroupBox(tr("Coordinates"));
-    QGridLayout *centerLayout = new QGridLayout(groupBox2);
+	groupBox2 = new QGroupBox(tr("Coordinates"));
+	QGridLayout *centerLayout = new QGridLayout(groupBox2);
 	centerLayout->addWidget( new QLabel(tr( "X (Columns)" )), 0, 1 );
 	centerLayout->addWidget( new QLabel(tr( "Y (Rows)" )), 0, 2 );
 
 	centerLayout->addWidget( new QLabel(tr( "First" )), 1, 0 );
 
 	QLocale locale = m->locale();
-    boxXStart = new DoubleSpinBox();
-    boxXStart->setLocale(locale);
-    centerLayout->addWidget( boxXStart, 1, 1 );
+	boxXStart = new DoubleSpinBox();
+	boxXStart->setLocale(locale);
+	centerLayout->addWidget( boxXStart, 1, 1 );
 
-    boxYStart = new DoubleSpinBox();
-    boxYStart->setLocale(locale);
+	boxYStart = new DoubleSpinBox();
+	boxYStart->setLocale(locale);
 	centerLayout->addWidget( boxYStart, 1, 2 );
 
 	centerLayout->addWidget( new QLabel(tr( "Last" )), 2, 0 );
-    boxXEnd = new DoubleSpinBox();
-    boxXEnd->setLocale(locale);
-    centerLayout->addWidget( boxXEnd, 2, 1 );
+	boxXEnd = new DoubleSpinBox();
+	boxXEnd->setLocale(locale);
+	centerLayout->addWidget( boxXEnd, 2, 1 );
 
-    boxYEnd = new DoubleSpinBox();
-    boxYEnd->setLocale(locale);
+	boxYEnd = new DoubleSpinBox();
+	boxYEnd->setLocale(locale);
 	centerLayout->addWidget( boxYEnd, 2, 2 );
 	centerLayout->setRowStretch(3, 1);
 
-    QHBoxLayout *bottomLayout = new QHBoxLayout();
-    bottomLayout->addStretch();
+	QHBoxLayout *bottomLayout = new QHBoxLayout();
+	bottomLayout->addStretch();
 	buttonApply = new QPushButton(tr("&Apply"));
 	buttonApply->setDefault( true );
 	bottomLayout->addWidget(buttonApply);
 	buttonOk = new QPushButton(tr("&OK"));
 	bottomLayout->addWidget( buttonOk );
-    buttonCancel = new QPushButton(tr("&Cancel"));
+	buttonCancel = new QPushButton(tr("&Cancel"));
 	bottomLayout->addWidget( buttonCancel );
 
 	QVBoxLayout * mainLayout = new QVBoxLayout( this );
-    mainLayout->addWidget(groupBox1);
+	mainLayout->addWidget(groupBox1);
 	mainLayout->addWidget(groupBox2);
 	mainLayout->addLayout(bottomLayout);
 
-    boxRows->setValue(m->numRows());
-    boxCols->setValue(m->numCols());
+	boxRows->setValue(m->numRows());
+	boxCols->setValue(m->numCols());
 
-    boxXStart->setValue(m->xStart());
+	boxXStart->setValue(m->xStart());
 	boxYStart->setValue(m->yStart());
 	boxXEnd->setValue(m->xEnd());
 	boxYEnd->setValue(m->yEnd());
 
 	connect( buttonApply, SIGNAL(clicked()), this, SLOT(apply()));
-    connect( buttonOk, SIGNAL(clicked()), this, SLOT(accept() ));
-    connect( buttonCancel, SIGNAL(clicked()), this, SLOT(reject()));
+	connect( buttonOk, SIGNAL(clicked()), this, SLOT(accept() ));
+	connect( buttonCancel, SIGNAL(clicked()), this, SLOT(reject()));
 }
 
 void MatrixSizeDialog::apply()
@@ -118,16 +118,16 @@ void MatrixSizeDialog::apply()
 	double toX = boxXEnd->value();
 	double fromY = boxYStart->value();
 	double toY = boxYEnd->value();
-    double oxs = d_matrix->xStart();
-    double oxe = d_matrix->xEnd();
-    double oys = d_matrix->yStart();
-    double oye = d_matrix->yEnd();
-    if(oxs != fromX || oxe != toX || oys != fromY || oye != toY){
-        d_matrix->undoStack()->push(new MatrixSetCoordinatesCommand(d_matrix,
-                        oxs, oxe, oys, oye, fromX, toX, fromY, toY,
-                        tr("Set Coordinates x[%1 : %2], y[%3 : %4]").arg(fromX).arg(toX).arg(fromY).arg(toY)));
-        d_matrix->setCoordinates(fromX, toX, fromY, toY);
-    }
+	double oxs = d_matrix->xStart();
+	double oxe = d_matrix->xEnd();
+	double oys = d_matrix->yStart();
+	double oye = d_matrix->yEnd();
+	if(oxs != fromX || oxe != toX || oys != fromY || oye != toY){
+		d_matrix->undoStack()->push(new MatrixSetCoordinatesCommand(d_matrix,
+						oxs, oxe, oys, oye, fromX, toX, fromY, toY,
+						tr("Set Coordinates x[%1 : %2], y[%3 : %4]").arg(fromX).arg(toX).arg(fromY).arg(toY)));
+		d_matrix->setCoordinates(fromX, toX, fromY, toY);
+	}
 	d_matrix->setDimensions(boxRows->value(), boxCols->value());
 }
 
