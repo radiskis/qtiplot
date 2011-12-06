7f1f61fd82c5168da552c4f2f0eef218070bc331
ion_vasilief <ion_vasilief@5a6a7de5-fb12-0410-b871-c33778c25c60>
2011-12-06 08:51:48 +0000

    Fixed some errors in the user manual.
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@2316 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/src/core/globals.h
+++ qtiplot/src/core/globals.h
@@ -36,9 +36,9 @@ const int min_version = 9;
 const int patch_version = 8;
 //! Extra version information string (like "alpha", "-2", etc...)
 const char * extra_version = ".10";
-const char * svn_revision = " svn 2315";//SVN_REVISION;  //SRB: SVN_REVISION set by compiler from QTIPLOT_SVN_REVISION environment variable. (10/1/2010 )
+const char * svn_revision = " svn 2316";//SVN_REVISION;  //SRB: SVN_REVISION set by compiler from QTIPLOT_SVN_REVISION environment variable. (10/1/2010 )
 
 //! Copyright string containing the author names
 const char * copyright_string = "Copyright (C) 2004-2011 Ion Vasilief";
 //! Release date as a string
-const char * release_date = " 2011/12/05";
+const char * release_date = " 2011/12/06";
