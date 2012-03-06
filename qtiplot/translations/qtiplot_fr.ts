ddde1328a7eb6851d9ecaeb59650348b8f937e39
claverie <claverie@5a6a7de5-fb12-0410-b871-c33778c25c60>
2012-03-06 06:42:22 +0000

    French translation updated
    git-svn-id: http://svn.berlios.de/svnroot/repos/qtiplot/trunk@2371 5a6a7de5-fb12-0410-b871-c33778c25c60

--- qtiplot/translations/qtiplot_fr.ts
+++ qtiplot/translations/qtiplot_fr.ts
@@ -6098,23 +6098,23 @@ Les codes spéciaux suivants peuvent être utilisés :
     </message>
     <message>
         <source>&amp;Speed Mode, Skip Points if needed</source>
-        <translation type="unfinished">Mode rapide, pa&amp;sser des points si nécessaire</translation>
+        <translation>Mode rapide, pa&amp;sser des points si nécessaire</translation>
     </message>
     <message>
         <source>A&amp;pply to curves with more than</source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;ppliquer aux courbes avec plus de</translation>
     </message>
     <message>
         <source>&amp;Tolerance (Douglas-Peucker algorithm)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Tolérance (algorithme de Douglas-Peucker)</translation>
     </message>
     <message>
         <source>0 (all data points)</source>
-        <translation type="unfinished">0 (tous les points)</translation>
+        <translation>0 (tous les points)</translation>
     </message>
     <message>
         <source>Apply to graphic &amp;export as well</source>
-        <translation type="unfinished"></translation>
+        <translation>Appliquer aux graphiques &amp;exportés</translation>
     </message>
 </context>
 <context>
